6239978fbe5e1d249e1bbdc780188956096b0f7d
Gina <1ginahong@gmail.com>
2019-06-28 12:49:00 -0700

    setup.py complete, can install and run package from console log

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='dlwebtoon',
+    version='1.0',
+    author='Gina Hong',
+    description='Download webtoons for offline reading!',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ginaahong/download_webtoons',
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': ['dlwebtoon=dlwebtoon.webtoon_dl:main'],
+    }
+)
